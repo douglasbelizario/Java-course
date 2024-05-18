cbdfad4f3c7795aaa387888d286afd56dd2fc8a6
douglasbelizario <doug0423@hotmail.com>
2024-05-18 10:50:17 -0300

    Product management exercise add

---
+++ poo_introduction/src/methods/product_management/v2/entities/Product.java
@@ -0,0 +1,31 @@
+package methods.product_management.v2.entities;
+
+public class Product {
+	
+	public String name;
+	public double price;
+	public int quantity;
+	
+	
+	public double priceToQuantity() {
+		return price * quantity;
+	}
+	
+	public void addQuantity(int quantity) {
+		this.quantity += quantity;
+	}
+	
+	public void removeQuantity(int quantity) {
+		this.quantity -= quantity;
+	}
+	
+	public String ToString() {
+				
+		return "Name: "+ name+
+				"\nPrice: $"+String.format("%.2f", price)+
+				"\nQuantity: $"+quantity+
+				"\nTotal: "+String.format("%.2f", priceToQuantity());
+				
+	}
+
+}
