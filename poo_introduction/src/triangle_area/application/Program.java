9cc001d0dc388f4585fea698c4fc9553b2d914e6
douglasbelizario <doug0423@hotmail.com>
2024-05-17 23:45:46 -0300

    First_commit

---
+++ poo_introduction/src/triangle_area/application/Program.java
@@ -0,0 +1,46 @@
+package triangle_area.application;
+
+import java.util.Scanner;
+
+import triangle_area.entities.Triangle;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Triangle x, y;
+		
+		x = new Triangle();
+		y = new Triangle();
+		
+		System.out.println("Informe os lados do triangulo X:");
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+		
+		System.out.println("Informe os lados do triangulo Y:");
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+		sc.close();
+		
+		double area1 = x.calcularArea();
+		double area2 = y.calcularArea();
+		
+		
+		System.out.printf("Triangulo X area: %.2f \n",area1);
+		System.out.printf("Triangulo Y area: %.2f \n",area2);
+		
+		if (area1 < area2) {
+			System.out.println("Segundo triangulo tem maior area");
+		} else if (area1 > area2){
+			System.out.println("Primeiro triangulo tem maior area");
+		} else {
+			System.out.println("São iguais");
+		}
+		
+	}
+
+}
---
+++ poo_introduction/src/triangle_area/entities/Triangle.java
@@ -0,0 +1,15 @@
+package triangle_area.entities;
+
+public class Triangle {
+	
+	public double a;
+	public double b;
+	public double c;
+	
+	public double calcularArea() {
+		double P = (a + b + c) / 2;
+		return Math.sqrt(P*(P-a)*(P-a)*(P-c));
+		
+	}
+	
+}
